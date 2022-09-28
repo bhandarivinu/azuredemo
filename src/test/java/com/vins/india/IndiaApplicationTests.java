9c847d1b2ef796efa225284ed9d083c65943ff4e
Vinay Kumar Bhandari (OSV) <vinay.bhandari.osv@fedex.com>
2022-09-28 09:35:38 +0530

    Vins commit

---
+++ src/test/java/com/vins/india/IndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vins.india;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
